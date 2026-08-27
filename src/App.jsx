875b2628cb42dcf73917696a91c1fe345fcacc91
naveedkhanNGU <photonk0348@gmail.com>
2026-08-27 18:23:23 +0500

    update the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
 
 const App = () => {
  const [theme, setTheme] = useState(
@@ -8,6 +10,8 @@ const App = () => {
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme}/>
+      <Hero/>
+      <TrustedBy/>
       
     </div>
   )
